a7a7ab653b87aa6102330f4051cffbed4aff6022
Duy Linh <cudotn111@gmail.com>
2022-07-20 22:26:50 +0700

    Duy Linh

---
+++ employeeCRUD/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DB_SEVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+
+
+$link = mysqli_connect(DB_SEVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($link === false) {
+    die("ERROR: Could not connect" .mysqli_connect_error());
+}
+
+
+?>
